60947dbd55b245c7526ef62d1ec5f75a6f68b327
hare <harupo.0963@icloud.com>
2022-03-14 00:35:59 +0900

    no message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,21 +27,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //ログインしているユーザー情報をviewに渡す
-        $user =\Auth::user();
-        //メモ一覧を取得 DESC降順 ASC昇順
-        $memos = Memo::where('user_id',$user['id'])->where('status',1)->orderBy('updated_at','DESC')->get();
         // dd($memos);
-        return view('home',compact('user','memos'));
+        return view('create');
     }
     
 
     public function create()
     {
-        //ログインしているユーザー情報をviewに渡す
-        $user =\Auth::user();
-        $memos = Memo::where('user_id',$user['id'])->where('status',1)->orderBy('updated_at','DESC')->get();
-        return view('create',compact('user','memos'));
+        
+        return view('create');
     }
 
 
@@ -82,14 +76,12 @@ class HomeController extends Controller
     public function edit($id){
         // 該当するIDのメモをデータベースから取得
         $user = \Auth::user();
+        
         $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])
           ->first();
         //   dd($memo);
-        $memos = Memo::where('user_id',$user['id'])->where('status',1)->orderBy('updated_at','DESC')->get();
-
-        $tags = Tag::where('user_id',$user['id'])->get();
         //取得したメモをViewに渡す
-        return view('edit',compact('memo','user','memos','tags'));
+        return view('edit',compact('memo'));
 
 
 
@@ -103,6 +95,18 @@ class HomeController extends Controller
         Memo::where('id',$id)->update(['content'=> $inputs['content'],'tag_id'=>$inputs['tag_id']]);
         return redirect()->route('home');
     }
+
+
+    public function delete(Request $request,$id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id',$id)->update([ 'status' => 2 ]);
+        return redirect()->route('home')->with('success','メモの削除が完了しました！');
+
+
+    }
+    
     
 
 }
